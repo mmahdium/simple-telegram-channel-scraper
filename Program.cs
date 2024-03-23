daac589a1615e91d263243a0aed4907e92c1a397
mahdium <mmm.6831.allah@gmail.com>
2024-03-23 23:44:24 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string channelId = "Nabz_e_shear"; // Replace with the actual channel ID
+            var scraper = new scraper.telegramScraper(channelId);
+            var latestMessages = scraper.GetLatestMessages();
+
+            using (var writer = new StreamWriter("../../../export.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(latestMessages);
+            }
+        }
+    }
+}
---
+++ tgmessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scraper
+{
+    public class tgmessage
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+    }
+}
